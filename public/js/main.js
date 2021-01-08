f8ea3f3324e45802f3a3d0d9b0b42e6f0cc36a19
Maxence <lemairemaxence@hotmail.com>
2021-01-08 09:31:47 +0100

    exo début

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+// EXO 1
+
+// let multiplicateur = 5;
+// let resultat;
+
+// for (let i = 1; i < 10; i++) {
+//     resultat = i * multiplicateur
+//     console.log(`${multiplicateur} * ${i} = ${resultat}`);
+// }
+
+// EXO 2
+
+// EXO 3
+
+let nb = 20
+let resultat;
+
+for (let i = 20; i < 10; i++) {
+    resultat = i - 2
+    console.log(resultat);
+}
+
+// EXO 4
+
+let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]
+
+let longsPrenoms = []
+
+// EXO 5
+
+let sommes = [14, 7 , 97, 65 , 247 , 67 , 101 , 4 , 34 ,78 , 1 ,900 , 0, 11 , 32 , 23 , 61];
+
+let [grossesSommes]
+
+// EXO 6
+
+let donnees = [14, 7 , 97, {} , "247" , 67 , 101 , true , 34 ,78 , [] ,'coding school' , 0, 11 , 32 , "italie" , 61,null,'cent',100];
+
+let typeString = []
+
+let typeNumber = []
+
+let typeObject = []
+
+let typeAutre = []
